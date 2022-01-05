ba846c5fbbb43ee5a3ebf428202c66822a40a165
Alexander Grosu <74964514+alexander-grosu@users.noreply.github.com>
2022-01-05 14:27:48 +0200

    Add files via upload

---
+++ Hibernate/src/main/java/Course.java
@@ -0,0 +1,37 @@
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+    @Entity
+    @Setter
+    @Getter
+    @Table(name = "Courses")
+    public class Course {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+        private Integer id;
+
+        private String name;
+
+        private Integer duration;
+
+        private String description;
+                                                  // 'int' is the primitive data type that cannot assign null to it
+        @Column(name = "teacher_id")              // while
+        private Integer teacherId;
+
+        @Column(name = "students_count")
+        private Integer studentsCount;
+
+        private Integer price;
+
+        @Column(name = "price_per_hour")  // - - - - -> name like in sql table
+        private float pricePerHour;
+
+        @Enumerated(EnumType.STRING)
+        @Column(columnDefinition = "enum")
+        private CourseType type;
+
+    }
